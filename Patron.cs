f2c4ba0f2e3d414a51e776e6df2615a16b825bfb
IanMcT <ian.mctavish@tldsb.on.ca>
2018-05-03 13:40:28 -0400

    Update Patron.cs

--- Patron.cs
+++ Patron.cs
@@ -5,54 +5,106 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.Windows.Threading;
 
 namespace Tapper
 {
-    class Patron
+    class Player
     {
-        Point[] parray = { new Point(200, 75), new Point(141, 184), new Point(79, 293), new Point(19, 402) };
-        Point pos;
-        int barNum;
-        int delay = 0;
-        double delayAmount;
-        int size = 50;
+        //Global variables
+        Point playerPos = new Point();
+        int counter = 0;
+        private Point point;
+        public Point Point { get => point; }
         Canvas canvas;
         Window window;
-        Rectangle sprite = new Rectangle();
-        Random r = new Random();
-        public Patron(Canvas c, Window w, int levelnum)
+        Rectangle playerRectangle;
+
+        public Player(Canvas c, Window w)
         {
-            delayAmount = 60 / levelnum;
+            //Generate player
             canvas = c;
             window = w;
-            barNum = r.Next(0, 4);
-            pos = parray[barNum];
-            sprite.Height = size;
-            sprite.Width = size;
-            ImageBrush darth = new ImageBrush(new BitmapImage(new Uri("darth.png",UriKind.Relative)));
-            sprite.Fill = darth;
-            Canvas.SetTop(sprite, pos.Y);
-            Canvas.SetLeft(sprite, pos.X);
-            canvas.Children.Add(sprite);
+            point = new Point(810, 406);
+            playerPos = point;
+            playerRectangle = new Rectangle();
+            playerRectangle.Fill = Brushes.White;
+            playerRectangle.Height = 100;
+            playerRectangle.Width = 100;
+            canvas.Children.Add(playerRectangle);
+            Canvas.SetTop(playerRectangle, point.Y);
+            Canvas.SetLeft(playerRectangle, point.X);
         }
         public void update()
         {
-            delay++;
-            if (delay >= delayAmount)
+            if (playerPos == new Point(630, 85)) //block on ascension to infinity
             {
-                delay = 0;
-                pos.X += size;
-                Canvas.SetLeft(sprite, pos.X);
+                if (Keyboard.IsKeyDown(Key.Down))
+                {
+                    point = new Point(point.X + 60, point.Y + 107);
+                    Canvas.SetTop(playerRectangle, point.Y);
+                    Canvas.SetLeft(playerRectangle, point.X);
+                    counter++;
+                    playerPos = point;
+                }
+            }
+            else if (playerPos == new Point(810, 406)) //block on decesion to infinity
+            {
+                if (Keyboard.IsKeyDown(Key.Up))
+                {
+                    point = new Point(point.X - 60, point.Y - 107);
+                    Canvas.SetTop(playerRectangle, point.Y);
+                    Canvas.SetLeft(playerRectangle, point.X);
+                    counter++;
+                    playerPos = point;
+                }
+            }
+            else //General movement
+            {
+                if (counter > 0) //Prevent constant movement
+                {
+                    if (Keyboard.IsKeyUp(Key.Up))
+                    {
+                        if (Keyboard.IsKeyUp(Key.Down))
+                        {
+                            counter = 0;
+                        }
+                    }
+                    if (Keyboard.IsKeyUp(Key.Down))
+                    {
+                        if (Keyboard.IsKeyUp(Key.Up))
+                        {
+                            counter = 0;
+                        }
+                    }
+                }
+                else //Re-installs player up or down depending on input
+                {
+                    if (Keyboard.IsKeyDown(Key.Up))
+                    {
+                        point = new Point(point.X - 60, point.Y - 107);
+                        Canvas.SetTop(playerRectangle, point.Y);
+                        Canvas.SetLeft(playerRectangle, point.X);
+                        counter++;
+                        playerPos = point;
+                    }
+                    if (Keyboard.IsKeyDown(Key.Down))
+                    {
+                        point = new Point(point.X + 60, point.Y + 107);
+                        Canvas.SetTop(playerRectangle, point.Y);
+                        Canvas.SetLeft(playerRectangle, point.X);
+                        counter++;
+                        playerPos = point;
+                    }
+                }
+            }
+            if (Keyboard.IsKeyDown(Key.Space))
+            {
+                MainWindow.addDrink();
+                counter++;
             }
         }
     }
 }
-
