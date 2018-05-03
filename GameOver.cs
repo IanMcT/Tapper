864cffb6e6d4354d56e1cfd6058fdac7cb517561
Brushey165732 <165732@tldsb.me>
2018-05-03 13:46:18 -0400

    Create GameOver.cs

---
+++ GameOver.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace u4_Tapper
+{
+    class GameOver
+    {
+        StreamReader streamReader = new StreamReader("highscores");
+        string line;
+        int score;
+        int counter;
+
+        Canvas canvas;
+        Window window;
+
+        public GameOver(Canvas c, Window w, int score)
+        {
+            window = w;
+            canvas = c;
+        }
+
+        public void getscores()
+        {
+            try
+            {
+                while (!streamReader.EndOfStream)
+                {
+                    string line = streamReader.ReadLine();
+                    int temp = line.IndexOf(" ");
+                    string pastscore = line.Substring(temp);
+                    
+
+                    int pastint = Convert.ToInt32(pastscore);
+
+                    if (score > pastint)
+                    {
+                        if (counter < 10)
+                        {
+                            MessageBox.Show("High Score!");
+
+
+                        }
+                    }
+                    else if (counter < 10)
+                    {
+                        counter++;
+                    }
+                    else
+                    {
+                        //Display high score list
+                    }
+                    
+                }
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
